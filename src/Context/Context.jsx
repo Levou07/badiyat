5b5f1725bd824c221a83afdb7cbb843abaa996fc
Levou7 <levou070@gmail.com>
2023-01-03 17:49:38 +0500

    new

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -5,9 +5,10 @@ const Context = createContext();
 function Provider ({children}){
 
     const [book, setBook] = useState([]);
+    const [sear, setSear] = useState([])
 
     return (
-        <Context.Provider value={{book, setBook}}>
+        <Context.Provider value={{book, setBook, sear, setSear}}>
             {children}
         </Context.Provider>
     )
--- src/components/First/Footer/Footer.jsx
+++ src/components/First/Footer/Footer.jsx
@@ -8,7 +8,10 @@ const category = []
 
 function Footer() {
     const {setBook} = useContext(Context)
+    const {setSear} = useContext(Context)
+
     const [id] = useState(0)
+    const [search, setSearch] = useState([])
 
     const [cate, setCate] = useState(category[0])
 
@@ -18,9 +21,20 @@ function Footer() {
         }
     })
 
+    const searchItem = (evn)=>{
+        const dataArr = []
+        jadidAdabiyoti?.map((item)=>{
+          if(item.name.toLocaleLowerCase().includes(evn.toLocaleLowerCase())){
+            dataArr.push(item)
+          }
+        })
+        setSearch(dataArr);
+    }
+
   return (
     <div>
         <div className="footer">
+            <input className='form-control' type="text" onChange={(e)=> searchItem(e.target.value)} placeholder='Adiblar, kitoblar, audiolar, maqolalar...' />
             <h2>Asosiy Kategoriyalar</h2>
             <ul className='cate'>
                 {
@@ -30,7 +44,31 @@ function Footer() {
                 };
             </ul>
             <ul className='footer-list'>
-                {
+                {   
+                    search.length > 0 ? search.map((e,i)=>(
+                        <Link key={i} to='/about'>                                
+                            <li key={i} id={id} onClick={()=>
+                            
+                                {
+                                    setBook(e);
+                                }
+                                
+                                }>
+                                <div className="card" style={{width: 173 + 'px', height: 236 + 'px'}}>
+                                    <img src={e.images} className="card-img-top" alt="img" />
+                                    <div className="card-body">
+                                        <h5 className="card-text">{e.name}</h5>
+                                        <p>{e.year}</p>
+                                    </div>
+                                    <div className="card-foot">
+                                        <button className="btn2"><i className="bi bi-journal"></i>34</button>
+                                        <button className="btn2"><i className="bi bi-earbuds"></i>13</button>
+                                    </div>
+                                </div>
+                            </li>
+                        </Link>
+                    ))
+                    :
                     jadidAdabiyoti.filter(e=> e.type === cate).map((e,i)=>(
                         <Link key={i} to='/about'>                                
                             <li key={i} id={id} onClick={()=> 
